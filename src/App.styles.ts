f04805c73a5853d94093c553c8834feaf8ffb83d
Yoshi <yoshi@mac-mini.local>
2021-11-14 23:42:42 +0900

    comp product

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -39,8 +39,33 @@ export const Wrapper = styled.div`
   h1 {
     font-family: Fascinate Inline, Haettenschmeiler, "Arial Narrow Bold",
       sans-serif;
-    background-image: linear-gradient(180def, #fff, #87f1ff);
+    background-image: linear-gradient(180deg, #fff, #87f1ff);
     background-size: 100%;
     background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+    filter: drop-shadow(2px 2px #0085a3);
+    font-size: 70px;
+    font-weight: 400;
+    text-align: center;
+    margin: 20px;
+  }
+
+  .start,
+  .next {
+    cursor: pointer;
+    background-color: linear-gradient(180deg, #fff, #ffcc91);
+    border: 2px solid #d38558;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    height: 40px;
+    margin: 20px;
+    padding: 0 40px;
+  }
+
+  .start {
+    max-width: 200px;
   }
 `;
